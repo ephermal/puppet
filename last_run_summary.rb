725f59bf7521dbad8e5a1951aa95327169e809b4
Gerald Reid <gerald.reid@gmail.com>
2020-01-20 09:10:21 -0800

    Update last_run_summary.rb

--- last_run_summary.rb
+++ last_run_summary.rb
@@ -1,7 +1,3 @@
-#Puppet fact to see if puppet ran or not; you can set a variable instead of puts; 
-#Use is for first run to do basic puppet (AD auth) to speed up provisioning
-
-
 require 'yaml'
 
 Facter.add(:last_run_summary) do
@@ -9,8 +5,10 @@ Facter.add(:last_run_summary) do
     lrs = YAML.load_file('/opt/puppetlabs/puppet/cache/state/last_run_summary.yaml')
     if lrs["events"]["total"] == 0 && lrs["events"]["failure"] == 0 && lrs["events"]["success"] == 0
     puts "Puppet never ran"
+    last_run_summary = 0
     else
     puts "Puppet ran; so doing full run"
+    last_run_summary = 1
     end
  end
 end
